6c4eaa307e3b83e106db43ffc67cb65ae5e91a16
Arthur Lee <archerlv99@gmail.com>
2022-08-24 16:33:37 +0600

    search bar and mock cell

--- MusicPlayApp/SearchView.swift
+++ MusicPlayApp/SearchView.swift
@@ -7,11 +7,50 @@
 
 import UIKit
 
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}
+
 class SearchView: UITableViewController {
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    private let tracks = [TrackModel(trackName: "some", artistName: "Alen"), TrackModel(trackName: "any", artistName: "Dem")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        setUpSearchBar()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+    }
+    
+    private func setUpSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = UIImage(named: "mock_image")
+        
+        return cell
+    }
+}
+
+extension SearchView: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 }
