882381a92b980008e608ab7d2593f43ee5fcfb6b
Arthur Lee <archerlv99@gmail.com>
2022-08-24 16:53:15 +0600

    SPM - alamofire set and search text request data written

--- MusicPlayApp/SearchView.swift
+++ MusicPlayApp/SearchView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 struct TrackModel {
     var trackName: String
@@ -52,5 +53,20 @@ extension SearchView: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
+        
+        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        
+        AF.request(url).responseData { dataResponse in
+            if let error = dataResponse.error {
+                print("error ercieved requesting data \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = dataResponse.data else { return }
+            
+            let someString = String(data: data, encoding: .utf8)
+            
+            print(someString ?? "")
+        }
     }
 }
